cd09bc7908baf5fc08f62832e4dd694e512fdf5b
chedom <chedom@syneforge.com>
2021-05-14 19:04:23 +0300

    init federation entity batch

--- pkg/engine/datasource/graphql_datasource/batch.go
+++ pkg/engine/datasource/graphql_datasource/batch.go
@@ -1,19 +1,26 @@
 package graphql_datasource
 
 import (
+	"fmt"
+
 	"github.com/jensneuse/graphql-go-tools/pkg/engine/resolve"
 )
 
 func ComposeFederationEntityBatch(fetches ...*resolve.SingleFetch) *resolve.SingleFetch {
-	var variables []resolve.Variables
+	var variables []resolve.Variable
 	var representations []string
+	var lastVariableIndex
 
 	for i, fetch := range fetches {
+		idx := fmt.Sprintf("[%d]", i)
+
 		for _, v := range fetch.Variables {
-			switch v.VariableKind() {
-			case resolve.VariableKindObject:
-				v
+			switch v := v.(type) {
+			case *resolve.ObjectVariable:
+				v.Path = append([]string{idx}, v.Path...)
 			}
+
+			variables = append(variables, v)
 		}
 	}
 
